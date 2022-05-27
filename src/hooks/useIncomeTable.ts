5566277709fb7d93b5acd6fb219e0bfa7d8ba814
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-05-27 18:43:46 +0900

    テーブルデータ作成hooks作成

--- src/hooks/useIncomeTable.ts
+++ src/hooks/useIncomeTable.ts
@@ -6,7 +6,12 @@ import axios from "axios";
 
 //他
 import Cookie from "universal-cookie";
-import { NestIncomeType, CategoryType } from "../types/MoneyType";
+import {
+  NestIncomeType,
+  CategoryType,
+  FamilyType,
+  NestUserType,
+} from "../types/MoneyType";
 
 export const useIncomeTable = (year: number, month: number) => {
   //ログイン中のユーザID
@@ -17,12 +22,15 @@ export const useIncomeTable = (year: number, month: number) => {
   const [dataCheck, setDataCheck] = useState(true);
 
   //テーブルに使用するデータ
-  const [tableData, setTableData] = useState<Array<CategoryType>>([]);
+  const [categoryTableData, setCategoryTableData] = useState<
+    Array<CategoryType>
+  >([]);
+  const [familyTableData, setFamilyTableData] = useState<Array<FamilyType>>([]);
 
   /**
    * 収入カテゴリテーブルデータ作成.
    */
-  const categoryTableData = useCallback(async () => {
+  const makeCategoryTableData = useCallback(async () => {
     //データがあるか否かを一旦trueに
     setDataCheck(true);
 
@@ -48,7 +56,59 @@ export const useIncomeTable = (year: number, month: number) => {
       });
     }
 
-    setTableData(dataArray);
+    setCategoryTableData(dataArray);
+
+    if (incomeData.length == 0) {
+      setDataCheck(false);
+    }
+  }, [year, month, userId]);
+
+  /**
+   * 収入データ取得.
+   * @remarks 家族データ
+   */
+  const makeFamilyTableData = useCallback(async () => {
+    //データがあるか否かを一旦trueに
+    setDataCheck(true);
+
+    const userData = await axios.get(`${apiUrl}/getuser/${userId}`);
+    const familyId = userData.data.user.familyId;
+    const result = await axios.post(`${apiUrl}/getfamily/insp/`, {
+      familyId: familyId,
+      year: year,
+      month: month,
+    });
+
+    const responseData = result.data.family;
+    const incomeData = new Array<NestUserType>();
+
+    //トータルが0になった時は排除するようにサーバ側で調整すること！
+    for (const responseItem of responseData) {
+      if (responseItem.incomeId.length > 0) {
+        incomeData.push(responseItem);
+      }
+    }
+
+    //円グラフに利用する配列
+    const dataArray = new Array<FamilyType>();
+
+    //支出情報を回して円グラフのデータを作成する
+    for (const item of incomeData) {
+      //値段データ
+      let totalPrice = 0;
+      for (const itemB of item.incomeId) {
+        totalPrice += itemB.total;
+      }
+
+      dataArray.push({
+        id: item._id,
+        name: item.name,
+        image: item.image,
+        price: totalPrice,
+      });
+    }
+
+    setFamilyTableData(dataArray);
 
     if (incomeData.length == 0) {
       setDataCheck(false);
@@ -57,7 +117,9 @@ export const useIncomeTable = (year: number, month: number) => {
 
   return {
     dataCheck,
+    makeCategoryTableData,
     categoryTableData,
-    tableData,
+    makeFamilyTableData,
+    familyTableData,
   };
 };
--- src/hooks/useSpendingTable.ts
+++ src/hooks/useSpendingTable.ts
@@ -6,15 +6,27 @@ import { apiUrl } from "../utils/values";
 
 //他
 import Cookie from "universal-cookie";
-import { NestSpendingType, CategoryType } from "../types/MoneyType";
+import {
+  NestSpendingType,
+  CategoryType,
+  NestUserType,
+  FamilyType,
+} from "../types/MoneyType";
 
+/**
+ * 支出データ取得.
+ * @remarks カテゴリ別
+ */
 export const useSpendingTable = (year: number, month: number) => {
   //ログイン中のユーザID
   const cookie = new Cookie();
   const [userId] = useState(cookie.get("userId"));
 
   //テーブルに使用するデータ
-  const [tableData, setTableData] = useState<Array<CategoryType>>([]);
+  const [categoryTableData, setCategoryTableData] = useState<
+    Array<CategoryType>
+  >([]);
+  const [familyTableData, setFamilyTableData] = useState<Array<FamilyType>>([]);
 
   //データがあるか否か
   const [dataCheck, setDataCheck] = useState(true);
@@ -22,7 +34,7 @@ export const useSpendingTable = (year: number, month: number) => {
   /**
    * 支出カテゴリテーブルデータ作成.
    */
-  const categoryTableData = useCallback(async () => {
+  const makeCategoryTableData = useCallback(async () => {
     //データがあるか否かを一旦trueに
     setDataCheck(true);
 
@@ -37,7 +49,7 @@ export const useSpendingTable = (year: number, month: number) => {
     //配列作成
     const dataArray = new Array<CategoryType>();
 
-    //支出情報を回して円グラフのデータを作成する
+    //支出情報を回してデータを作成する
     for (const item of spendingData) {
       dataArray.push({
         id: item._id,
@@ -48,7 +60,58 @@ export const useSpendingTable = (year: number, month: number) => {
       });
     }
 
-    setTableData(dataArray);
+    setCategoryTableData(dataArray);
+
+    if (spendingData.length == 0) {
+      setDataCheck(false);
+    }
+  }, [year, month, userId]);
+
+  /**
+   * 支出データ取得.
+   * @remarks 家族データ
+   */
+  const makeFamilyTableData = useCallback(async () => {
+    //データがあるか否かを一旦trueに
+    setDataCheck(true);
+
+    const userData = await axios.get(`${apiUrl}/getuser/${userId}`);
+    const familyId = userData.data.user.familyId;
+    const result = await axios.post(`${apiUrl}/getfamily/insp/`, {
+      familyId: familyId,
+      year: year,
+      month: month,
+    });
+
+    const responseData = result.data.family;
+    const spendingData = new Array<NestUserType>();
+
+    //トータルが0になった時は排除するようにサーバ側で調整すること！
+    for (const responseItem of responseData) {
+      if (responseItem.spendingId.length > 0) {
+        spendingData.push(responseItem);
+      }
+    }
+
+    const dataArray = new Array<FamilyType>();
+
+    //支出情報を回してデータを作成する
+    for (const item of spendingData) {
+      //値段データ
+      let totalPrice = 0;
+      for (const itemB of item.spendingId) {
+        totalPrice += itemB.total;
+      }
+
+      dataArray.push({
+        id: item._id,
+        name: item.name,
+        image: item.image,
+        price: totalPrice,
+      });
+    }
+
+    setFamilyTableData(dataArray);
 
     if (spendingData.length == 0) {
       setDataCheck(false);
@@ -57,7 +120,9 @@ export const useSpendingTable = (year: number, month: number) => {
 
   return {
     dataCheck,
+    makeCategoryTableData,
     categoryTableData,
-    tableData,
+    makeFamilyTableData,
+    familyTableData,
   };
 };
