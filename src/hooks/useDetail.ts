fb7351f92219bf572f80a5cbfb8d3bb2e5069ff0
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-05-30 10:00:56 +0900

    グループデータ取得メソッド追加

--- src/hooks/useDetail.ts
+++ src/hooks/useDetail.ts
@@ -4,11 +4,16 @@ import { useCallback, useState } from "react";
 import Cookie from "universal-cookie";
 import {
   CategoryDetailType,
+  FamilyDetailType,
   NestIncomeType,
   NestSpendingType,
+  NestUserType,
 } from "../types/MoneyType";
 import { apiUrl } from "../utils/values";
 
+/**
+ * 支出個人データ取得.
+ */
 export const useSpendingDetail = (year: number, month: number) => {
   //ログイン中のユーザID
   const cookie = new Cookie();
@@ -17,6 +22,9 @@ export const useSpendingDetail = (year: number, month: number) => {
   //テーブルデータ
   const [spendingCategoryTable, setSpendingCategoryTable] = useState<
     Array<CategoryDetailType>
+  >([]);
+  const [spendingFamilyTable, setSpendingFamilyTable] = useState<
+    Array<FamilyDetailType>
   >([]);
 
   //データがあるか否か
@@ -65,17 +73,75 @@ export const useSpendingDetail = (year: number, month: number) => {
     }
   }, [year, month, spendingDataCheck]);
 
+  /**
+   * グループ支出アイテム取得.
+   */
+  const getFamilySpending = useCallback(async () => {
+    //データがあるか否かを一旦trueに
+    setSpendingDataCheck(true);
+
+    //API
+    const user = await axios.get(`${apiUrl}/getuser/${userId}`);
+    const familyId = user.data.user.familyId;
+    const result = await axios.post(`${apiUrl}/getfamily/insp/`, {
+      familyId: familyId,
+      year: year,
+      month: month,
+    });
+
+    const responseData = result.data.family;
+    const userData = new Array<NestUserType>();
+
+    //トータルが0になった時は排除するようにサーバ側で調整すること！
+    for (const responseItem of responseData) {
+      if (responseItem.spendingId.length > 0) {
+        userData.push(responseItem);
+      }
+    }
+
+    const dataArray = new Array<FamilyDetailType>();
+
+    //データの作成
+    for (const userItem of userData) {
+      for (const spendingItem of userItem.spendingId) {
+        for (const item of spendingItem.spendingId) {
+          dataArray.push({
+            id: spendingItem._id,
+            name: userItem.name,
+            contentName: item.name,
+            image: userItem.image,
+            price: item.howmatch,
+            createdAt: item.createdAt,
+          });
+        }
+      }
+    }
+
+    //日付順に並び替え
+    dataArray.sort((a, b) => {
+      return a.createdAt > b.createdAt ? 1 : -1;
+    });
+
+    setSpendingFamilyTable(dataArray);
+
+    //データがなければfalseにする
+    if (dataArray.length == 0) {
+      setSpendingDataCheck(false);
+    }
+  }, [year, month, spendingDataCheck]);
+
   return {
     spendingDataCheck,
     getCategorySpending,
     spendingCategoryTable,
+    getFamilySpending,
+    spendingFamilyTable,
   };
 };
 
 /**
- * 収入データ取得.
- * @param year 年
- * @param month 月
+ * 収入個人データ取得.
+ *
  */
 export const useIncomeDetail = (year: number, month: number) => {
   //ログイン中のユーザID
@@ -85,6 +151,9 @@ export const useIncomeDetail = (year: number, month: number) => {
   //テーブルデータ
   const [incomeCategoryTable, setIncomeCategoryTable] = useState<
     Array<CategoryDetailType>
+  >([]);
+  const [incomeFamilyTable, setIncomeFamilyTable] = useState<
+    Array<FamilyDetailType>
   >([]);
 
   //データがあるか否か
@@ -133,9 +202,68 @@ export const useIncomeDetail = (year: number, month: number) => {
     }
   }, [year, month, incomeDataCheck]);
 
+  /**
+   * グループ収入アイテム取得.
+   */
+  const getFamilyIncome = useCallback(async () => {
+    //データがあるか否かを一旦trueに
+    setIncomeDataCheck(true);
+
+    //API
+    const user = await axios.get(`${apiUrl}/getuser/${userId}`);
+    const familyId = user.data.user.familyId;
+    const result = await axios.post(`${apiUrl}/getfamily/insp/`, {
+      familyId: familyId,
+      year: year,
+      month: month,
+    });
+
+    const responseData = result.data.family;
+    const userData = new Array<NestUserType>();
+
+    //トータルが0になった時は排除するようにサーバ側で調整すること！
+    for (const responseItem of responseData) {
+      if (responseItem.spendingId.length > 0) {
+        userData.push(responseItem);
+      }
+    }
+
+    const dataArray = new Array<FamilyDetailType>();
+
+    //データの作成
+    for (const userItem of userData) {
+      for (const incomeItem of userItem.incomeId) {
+        for (const item of incomeItem.incomeId) {
+          dataArray.push({
+            id: incomeItem._id,
+            name: userItem.name,
+            contentName: item.name,
+            image: userItem.image,
+            price: item.howmatch,
+            createdAt: item.createdAt,
+          });
+        }
+      }
+    }
+
+    //日付順に並び替え
+    dataArray.sort((a, b) => {
+      return a.createdAt > b.createdAt ? 1 : -1;
+    });
+
+    setIncomeFamilyTable(dataArray);
+
+    //データがなければfalseにする
+    if (dataArray.length == 0) {
+      setIncomeDataCheck(false);
+    }
+  }, [year, month, incomeDataCheck]);
+
   return {
     incomeDataCheck,
     getCategoryIncome,
     incomeCategoryTable,
+    getFamilyIncome,
+    incomeFamilyTable,
   };
 };
