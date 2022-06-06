cabe40613db7044e110dda357dd949bd04d29bd6
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-06-06 14:58:48 +0900

    家族情報編集画面実装

--- src/pages/group/edit.tsx
+++ src/pages/group/edit.tsx
@@ -1,96 +1,64 @@
 import styled from "@emotion/styled";
 import { Button } from "@mui/material";
-import type { NextPage } from "next";
+import type {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
 import { InputText } from "../../components/form/InputText";
 import { PageLayout } from "../../components/layout/PageLayout";
-import { PageTitle } from "../../components/layout/PageTitle";
+import { useRouter } from "next/router";
 
 import SendIcon from "@mui/icons-material/Send";
 import { useState, useCallback } from "react";
+import Cookies from "universal-cookie";
+import { apiUrl } from "../../utils/values";
+import { FamilyType } from "../../types/UserType";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 /**
  * 家族情報編集.
  */
-const FamilyEdit: NextPage = () => {
-  /**
-   * 家族情報の取得.
-   */
-
-  const familyMember = [];
-
-  // const familyMember = [
-  //   {
-  //     id: "00000",
-  //     image: "/book-tab-logo-face.jpg",
-  //     familyID: "1234567890",
-  //     name: "山田千代子",
-  //     mail: "yamada@mail.com",
-  //     password: "yamayama",
-  //     role: "母",
-  //   },
-  //   {
-  //     id: "11111",
-  //     image: "/book-tab-logo-face.jpg",
-  //     familyID: "1234567890",
-  //     name: "山田花子",
-  //     mail: "yamada@mail.com",
-  //     password: "yamayama",
-  //     role: "娘",
-  //   },
-  //   {
-  //     id: "22222",
-  //     image: "/book-tab-logo-face.jpg",
-  //     familyID: "1234567890",
-  //     name: "山田五郎",
-  //     mail: "yamada@mail.com",
-  //     password: "yamayama",
-  //     role: "息子",
-  //   },
-  //   {
-  //     id: "33333",
-  //     image: "/book-tab-logo-face.jpg",
-  //     familyID: "1234567890",
-  //     name: "山田千代子",
-  //     mail: "yamada@mail.com",
-  //     password: "yamayama",
-  //     role: "母",
-  //   },
-  //   {
-  //     id: "4444",
-  //     image: "/book-tab-logo-face.jpg",
-  //     familyID: "1234567890",
-  //     name: "山田花子",
-  //     mail: "yamada@mail.com",
-  //     password: "yamayama",
-  //     role: "娘",
-  //   },
-  //   {
-  //     id: "55555",
-  //     image: "/book-tab-logo-face.jpg",
-  //     familyID: "1234567890",
-  //     name: "山田五郎",
-  //     mail: "yamada@mail.com",
-  //     password: "yamayama",
-  //     role: "息子",
-  //   },
-  // ];
+const FamilyEdit: NextPage<Props> = ({ familyData }) => {
+  const router = useRouter();
+  //家族情報
+  const familyItem: FamilyType = familyData.family;
 
   //名前
-  const [name, setName] = useState<string>("");
+  const [name, setName] = useState<string>(familyItem.name);
   const [nameError, setNameError] = useState<string>("");
 
   /**
    * DBにユーザ登録.
    */
-  const postUserData = useCallback(() => {
+  const postUserData = useCallback(async () => {
+    //初期化
+    setNameError("");
+    let error = "";
+
     if (name === "") {
       setNameError("グループ名を入力して下さい。");
+      error = "エラーあり";
     }
 
-    if (nameError !== "") {
+    if (error !== "") {
       return;
     }
-  }, []);
+
+    try {
+      await axios.post(`${apiUrl}/updatefamily/${familyItem._id}`, {
+        name: name,
+      });
+      toast.success("登録しました。");
+      router.push("/user/");
+    } catch (e) {
+      toast.error("更新出来ませんでした。" + e);
+    }
+  }, [name, nameError]);
 
   return (
     <>
@@ -117,6 +85,29 @@ const FamilyEdit: NextPage = () => {
       </PageLayout>
     </>
   );
+};
+
+/**
+ * SSRで初期データ取得.
+ * @returns 家族情報データ
+ */
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  const cookies = new Cookies(ctx.req.headers.cookie);
+  const userId = cookies.get("userId");
+  const userRes = await fetch(`${apiUrl}/getuser/${userId}`);
+  const userData = await userRes.json();
+
+  const familyRes = await fetch(
+    `${apiUrl}/getfamily/${userData.user.familyId}`
+  );
+
+  const familyData = await familyRes.json();
+
+  return {
+    props: { familyData },
+  };
 };
 
 //テキストボックス1つ1つ
