bcb3e7ef9ffc1b537f0a5964dc2cbc8848f0b3c2
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-06-06 12:26:17 +0900

    家族データ新規登録メソッド実装

--- src/pages/group/new.tsx
+++ src/pages/group/new.tsx
@@ -1,15 +1,30 @@
-import styled from "@emotion/styled";
-import { Button } from "@mui/material";
-import type { NextPage } from "next";
 import { useState, useCallback } from "react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+
+//MUI
+import styled from "@emotion/styled";
+import SendIcon from "@mui/icons-material/Send";
+import { Button } from "@mui/material";
+
+//components
 import { InputText } from "../../components/form/InputText";
 import { PageLayout } from "../../components/layout/PageLayout";
-import SendIcon from "@mui/icons-material/Send";
+import { apiUrl } from "../../utils/values";
+
+//others
+import axios from "axios";
+import toast from "react-hot-toast";
+import Cookie from "universal-cookie";
 
 /**
  * 家族新規登録.
  */
 const NewFamily: NextPage = () => {
+  const cookie = new Cookie();
+  const userId = cookie.get("userId");
+  const router = useRouter();
+
   //名前
   const [name, setName] = useState<string>("");
   const [nameError, setNameError] = useState<string>("");
@@ -29,31 +44,62 @@ const NewFamily: NextPage = () => {
   /**
    * DBにユーザ登録.
    */
-  const postUserData = useCallback(() => {
+  const postUserData = useCallback(async () => {
+    //エラー値初期化
+    setNameError("");
+    setPasswordError("");
+    setScretWordError("");
+    setConfirmPasswordError("");
+    let error = "";
+
     if (name === "") {
       setNameError("グループ名を入力して下さい。");
+      error = "エラーあり";
     }
 
     if (secretWord === "") {
       setScretWordError("合言葉を入力して下さい。");
+      error = "エラーあり";
     }
 
     if (password === "") {
       setPasswordError("パスワードを入力して下さい。");
+      error = "エラーあり";
     }
 
-    if (password === confirmPassword) {
+    if (password !== confirmPassword) {
       setConfirmPasswordError("パスワードと異なっています。");
+      error = "エラーあり";
     }
-    if (
-      nameError !== "" ||
-      passwordError !== "" ||
-      secretWordError !== "" ||
-      confirmPasswordError !== ""
-    ) {
+    if (error !== "") {
       return;
     }
-  }, []);
+
+    //送るデータ作成
+    const postData = {
+      userId: userId,
+      name: name,
+      secretWord: secretWord,
+      password: password,
+    };
+
+    try {
+      await axios.post(`${apiUrl}/addfamily`, postData);
+      toast.success(name + "を登録しました。");
+      router.push("/user/");
+    } catch (e) {
+      toast.error("登録に失敗しました。" + e);
+    }
+  }, [
+    name,
+    password,
+    confirmPassword,
+    secretWord,
+    nameError,
+    passwordError,
+    confirmPasswordError,
+    secretWordError,
+  ]);
 
   return (
     <>
@@ -82,6 +128,7 @@ const NewFamily: NextPage = () => {
             value={password}
             setWord={setPassword}
             errorItem={passwordError}
+            type="password"
           />
         </_TextInput>
 
@@ -91,6 +138,7 @@ const NewFamily: NextPage = () => {
             value={confirmPassword}
             setWord={setConfirmPassword}
             errorItem={confirmPasswordError}
+            type="password"
           />
         </_TextInput>
 
