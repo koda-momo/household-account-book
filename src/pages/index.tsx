573c241b72bf0ee5a09afc6a01ff18d47877f3a8
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-06-07 10:37:19 +0900

    トップ画面完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,84 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
+import { useState, useCallback } from "react";
 import Image from "next/image";
-import useSWR from "swr";
+import { useRouter } from "next/router";
 
 //MUI
 import { styled } from "@mui/material/styles";
-import { CircularProgress } from "@mui/material";
+import { Button } from "@mui/material";
 
 //others
-import { fetcher } from "../utils/fetcher";
+import Cookies from "universal-cookie";
+import { UserType } from "../types/UserType";
+import { apiUrl } from "../utils/values";
+
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 /**
  * スタートアップページ.
  */
-const StartPage: NextPage = () => {
-  const { data, error } = useSWR(
-    "https://weather.tsukumijima.net/api/forecast/city/400040",
-    fetcher
-  );
+const StartPage: NextPage<Props> = ({ userData }) => {
+  const [userName] = useState(userData.user.name);
+  const router = useRouter();
 
-  const [place, setPlace] = useState<string>("");
-  const [weather, setWeather] = useState<string>("");
-
-  useEffect(() => {
-    if (data) {
-      setPlace(data.title);
-      setWeather(data.forecasts[0].telop);
-    }
-  }, [data]);
-
-  if (error) return <div>failed to load</div>;
-  if (place === "" && weather === "")
-    return (
-      <_Loading>
-        <CircularProgress />
-      </_Loading>
-    );
+  /**
+   * トップページへ.
+   */
+  const goToTop = useCallback(() => {
+    router.push("/top/");
+  }, []);
 
   return (
     <>
       <_Flex>
         <_Image>
-          <Image src="/images/toragao.png" width={300} height={300} />
+          <Image src="/images/toragao.png" width={250} height={250} />
         </_Image>
         <_Greeting>
-          <div>おはよう、山田太郎</div>
-          <div>
-            今日の{place}は{weather}です
-          </div>
-          <div>よき1日を</div>
+          <div>おはよう、{userName}</div>
+          <div>今日もよき1日を</div>
         </_Greeting>
       </_Flex>
+
+      <_Button>
+        <Button variant="contained" onClick={goToTop} color="primary">
+          家計簿トップページへ
+        </Button>
+      </_Button>
     </>
   );
+};
+
+/**
+ * SSRで初期データ取得.
+ * @returns ユーザ情報初期表示用データ
+ */
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  const cookies = new Cookies(ctx.req.headers.cookie);
+  const userId = cookies.get("userId");
+  const userRes = await fetch(`${apiUrl}/getuser/${userId}`);
+  const userData: UserType = await userRes.json();
+
+  return {
+    props: { userData },
+  };
 };
 
 const _Flex = styled("div")(() => ({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  marginTop: 50,
+  gap: 20,
+  "@media screen and (max-width:600px)": {
+    gap: 0,
+  },
 }));
 
 const _Image = styled("div")(() => ({
@@ -70,21 +89,16 @@ const _Image = styled("div")(() => ({
 }));
 
 const _Greeting = styled("div")(() => ({
+  fontSize: 20,
   "@media screen and (max-width:600px)": {
-    fontSize: 10,
+    width: "50%",
+    fontSize: 15,
   },
 }));
 
-const _Loading = styled("div")(() => ({
-  display: "flex",
-  justifyContent: "center",
-  marginBottom: 50,
-  width: 500,
-  height: 500,
-  "@media screen and (max-width:600px)": {
-    width: 300,
-    height: 300,
-  },
+const _Button = styled("div")(() => ({
+  textAlign: "center",
+  marginTop: 10,
 }));
 
 export default StartPage;
